87f0ced5686273c35ca3368179eab8c2fbcfb5e7
Keith Gabriel Badulis <keith_gabriel_badulis@dlsu.edu.ph>
2021-11-05 13:49:03 +0800

    removed 500

--- src/components/RepeatForm.vue
+++ src/components/RepeatForm.vue
@@ -14,7 +14,7 @@
 
     <h5>Remember!</h5>
     <p>
-      100 points is equivalent to one raffle ticket for a for a chance to win Php 250.00 or Php 500.00 through GCash <br>
+      100 points is equivalent to one raffle ticket for a for a chance to win Php 250.00 through GCash <br>
       You can answer the form again to label new images and earn more points!
     </p>
 
